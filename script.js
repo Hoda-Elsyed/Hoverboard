5077c79ffdb790b43495f9b2cccde47e83860a60
Hoda-Elsayed-Mohamed <96476988+Hoda-Elsayed-Mohamed@users.noreply.github.com>
2023-02-07 17:19:34 +0200

    c

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function createBoxes(){
     }
     
 function lightenUp(e){
-    e.stopImmediatePropagation()
     let randomColor ='#' + Math.floor(Math.random()*16777215).toString(16);
     e.target.style.backgroundColor = randomColor
     console.log(randomColor);
